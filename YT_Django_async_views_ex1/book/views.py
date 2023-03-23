8737f7f9ec7501ba09c0a93fe7a51cafca7841cd
andrefimenko <andrefimenko@yandex.ru>
2023-03-24 01:12:47 +0300

    17:26

--- YT_Django_async_views_ex1/book/views.py
+++ YT_Django_async_views_ex1/book/views.py
@@ -27,18 +27,36 @@ from django.shortcuts import render
 #         {"data": pokemon_data, "count": count, "time": total_time},
 #     )
 
+# async def example(request):
+#
+#     starting_time = time.time()
+#
+#     pokemon_data = []
+#
+#     async with aiohttp.ClientSession() as session:
+#         for num in range(1, 101):
+#             pokemon_url = f"https://pokeapi.co/api/v2/pokemon/{num}"
+#             async with session.get(pokemon_url) as res:
+#                 pokemon = await res.json()
+#                 pokemon_data.append(pokemon["name"])
+#
+#     count = len(pokemon_data)
+#     total_time = time.time() - starting_time
+#
+#     return render(
+#         request,
+#         "index.html",
+#         {"data": pokemon_data, "count": count, "time": total_time},
+#     )
+
+
+
+
 async def example(request):
 
     starting_time = time.time()
 
     pokemon_data = []
-
-    async with aiohttp.ClientSession() as session:
-        for num in range(1, 101):
-            pokemon_url = f"https://pokeapi.co/api/v2/pokemon/{num}"
-            async with session.get(pokemon_url) as res:
-                pokemon = await res.json()
-                pokemon_data.append(pokemon["name"])
 
     count = len(pokemon_data)
     total_time = time.time() - starting_time
